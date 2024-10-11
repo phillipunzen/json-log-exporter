40e558c94502d746edd7785d06d0a8e798f3dbda
Phillip Unzen <58786365+phillipunzen@users.noreply.github.com>
2024-10-11 10:58:31 +0200

    Update main.go

--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"flag"
-	"github.com/ashwinikd/json-log-exporter/collector"
-	"github.com/ashwinikd/json-log-exporter/config"
+	"github.com/phillipunzen/json-log-exporter/collector"
+	"github.com/phillipunzen/json-log-exporter/config"
 	"log"
 	"net"
 	"net/http"
